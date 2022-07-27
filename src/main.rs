380c8791374d012d28c38dd9afc5e6cca0baafa7
matcool <26722564+matcool@users.noreply.github.com>
2022-07-27 15:25:46 -0300

    add tests

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use compiler::Compiler;
 use graph::GraphGen;
 use lexer::*;
 use parser::Parser;
+use path_slash::PathBufExt;
 
 fn print_help_and_exit() -> ! {
 	println!(
@@ -53,6 +54,10 @@ fn main() {
 		}
 	}
 
+	run(input, output, Some("graph.gv".into()), build);
+}
+
+fn run<S: AsRef<std::path::Path>>(input: S, output_path: Option<String>, graph_file: Option<String>, build: bool) {
 	let contents = std::fs::read_to_string(input).unwrap();
 
 	let mut lexer = Lexer::new(contents.chars().peekable());
@@ -65,31 +70,67 @@ fn main() {
 	checker.check().unwrap();
 
 	// println!("{:#?}", parser.functions);
-	std::fs::write("graph.gv", GraphGen::generate_graph(&parser).unwrap()).unwrap();
+	if let Some(path) = graph_file {
+		std::fs::write(path, GraphGen::generate_graph(&parser).unwrap()).unwrap();
+	}
 
 	let compiler = Compiler::new(parser);
 
 	let asm_output = compiler.compile();
 
-	if let Some(output_path) = output {
-		std::fs::write(
-			&output_path,
-			include_str!("nasm.asm").to_string() + &asm_output,
-		).unwrap();
+	if let Some(output) = output_path {
+		std::fs::write(&output, include_str!("nasm.asm").to_string() + &asm_output).unwrap();
 		if build {
+			// use bash -c because im on windows and i want it to run on wsl :-)
 			Command::new("bash")
 				.arg("-c")
-				.arg(format!("nasm -f elf \"{0}\" -o \"{0}.o\"", output_path))
+				.arg(format!("nasm -f elf \"{0}\" -o \"{0}.o\"", output))
 				.output()
 				.unwrap();
 
 			Command::new("bash")
 				.arg("-c")
-				.arg(format!("ld -m elf_i386 \"{0}.o\" -o \"{0}\"", output_path))
+				.arg(format!("ld -m elf_i386 \"{0}.o\" -o \"{0}\"", output))
 				.output()
 				.unwrap();
 		}
 	} else {
 		print!("Compiler output:\n{}", asm_output);
+	}
+}
+
+#[test]
+fn run_tests() {
+	let build_path = std::path::Path::new("tests/build");
+	// ignore if folder already exists
+	let _ = std::fs::create_dir(build_path);
+	for folder in std::fs::read_dir("tests").unwrap() {
+		let folder = folder.unwrap();
+		if folder.path().is_dir() && folder.file_name() != "build" {
+			for file in std::fs::read_dir(folder.path()).unwrap() {
+				let file = file.unwrap();
+				print!("{} - ", file.path().to_slash().unwrap());
+				assert!(file.file_name().to_str().unwrap().to_string().ends_with(".tack"), "Unknown file in test folder: {:?}", file.file_name());
+				// oops this keeps the .tack file ext.. oh well
+				let binary_path = build_path.join(format!(
+					"{}__{}",
+					folder.file_name().to_str().unwrap(),
+					file.file_name().to_str().unwrap()
+				));
+
+				run(
+					file.path(),
+					Some(binary_path.to_slash().unwrap().to_string()),
+					None,
+					true,
+				);
+				let out = Command::new("bash")
+					.arg("-c")
+					.arg(format!("./{}", binary_path.to_slash().unwrap()))
+					.output()
+					.unwrap();
+				println!("returned code {}, output: {:?}", out.status.code().unwrap(), out.stdout);
+			}
+		}
 	}
 }
