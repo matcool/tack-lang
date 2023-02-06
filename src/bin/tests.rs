a445070b328abe51cab72fd5dd305068aa0d3b70
matcool <26722564+matcool@users.noreply.github.com>
2023-02-06 15:35:18 -0300

    add option to run single test

--- src/bin/tests.rs
+++ src/bin/tests.rs
@@ -1,82 +1,86 @@
 use path_slash::PathBufExt;
 use std::{
 	io::{BufRead, BufReader},
-	process::Command,
+	process::Command, path::PathBuf,
 };
 
 use tack::run::run;
+
+fn run_test(path: PathBuf, binary_path: PathBuf) {
+	let mut expected_code = None;
+	for line in BufReader::new(std::fs::File::open(path.clone()).unwrap()).lines() {
+		let line = line.unwrap();
+		let Some((_, comment)) = line.split_once("// ") else {
+			break;
+		};
+		let Some((key, value)) = comment.split_once(' ') else {
+			break;
+		};
+		if key == "returns" {
+			expected_code = Some(value.parse::<i32>().unwrap());
+		} else {
+			break;
+		}
+	}
+
+	run(
+		path,
+		Some(binary_path.to_slash().unwrap().to_string()),
+		None,
+		true,
+	);
+
+	let out = Command::new("bash")
+		.arg("-c")
+		.arg(format!("./{}", binary_path.to_slash().unwrap()))
+		.output()
+		.unwrap();
+	let code = out.status.code().unwrap();
+	print!("returned code {code} ");
+	if !out.stdout.is_empty() {
+		print!(" output: {:?} ", out.stdout);
+	}
+	if code == 11 {
+		print!("SEGFAULT");
+	} else if let Some(expected_code) = expected_code {
+		if expected_code == code {
+			print!("OK");
+		} else {
+			print!("FAIL (expected {expected_code})");
+		}
+	}
+
+}
+
 
 fn main() {
 	let build_path = std::path::Path::new("tests/build");
 	// ignore if folder already exists
 	let _ = std::fs::create_dir(build_path);
-	for folder in std::fs::read_dir("tests").unwrap() {
-		let folder = folder.unwrap();
-		if folder.path().is_dir() && folder.file_name() != "build" {
-			for file in std::fs::read_dir(folder.path()).unwrap() {
-				let file = file.unwrap();
-				print!("{} - ", file.path().to_slash().unwrap());
-				assert!(
-					file.file_name()
-						.to_str()
-						.unwrap()
-						.to_string()
-						.ends_with(".tack"),
-					"Unknown file in test folder: {:?}",
-					file.file_name()
-				);
-
-				// oops this keeps the .tack file ext.. oh well
-				let binary_path = build_path.join(format!(
-					"{}__{}",
-					folder.file_name().to_str().unwrap(),
-					file.file_name().to_str().unwrap()
-				));
-
-				let mut expected_code = None;
-				{
-					let file = std::fs::File::open(file.path()).unwrap();
-					for line in BufReader::new(file).lines() {
-						let line = line.unwrap();
-						if let Some((_, comment)) = line.split_once("// ") {
-							let (key, value) = comment.split_once(' ').unwrap();
-							if key == "returns" {
-								expected_code = Some(value.parse::<i32>().unwrap());
-							} else {
-								break;
-							}
-						} else {
-							break;
-						}
+	if let Some(path) = std::env::args().skip(1).next() {
+		run_test(std::path::Path::new("tests").join(path), build_path.join("foo"));
+	} else {
+		for folder in std::fs::read_dir("tests").unwrap() {
+			let folder = folder.unwrap();
+			if folder.path().is_dir() && folder.file_name() != "build" {
+				for file in std::fs::read_dir(folder.path()).unwrap() {
+					let file = file.unwrap();
+					print!("{} - ", file.path().to_slash().unwrap());
+					let file_name = file.file_name().into_string().unwrap();
+					if !file_name.ends_with(".tack") {
+						println!("Skipping unknown file in test folder");
+						continue;
 					}
+	
+					let binary_path = build_path.join(format!(
+						"{}__{}",
+						folder.file_name().to_str().unwrap(),
+						file_name.strip_suffix(".tack").unwrap()
+					));
+	
+					run_test(file.path(), binary_path);
+					println!();
 				}
-
-				run(
-					file.path(),
-					Some(binary_path.to_slash().unwrap().to_string()),
-					None,
-					true,
-				);
-				let out = Command::new("bash")
-					.arg("-c")
-					.arg(format!("./{}", binary_path.to_slash().unwrap()))
-					.output()
-					.unwrap();
-				let code = out.status.code().unwrap();
-				print!("returned code {code} ");
-				if !out.stdout.is_empty() {
-					print!(" output: {:?} ", out.stdout);
-				}
-				if code == 11 {
-					print!("SEGFAULT");
-				} else if let Some(expected_code) = expected_code {
-					if expected_code == code {
-						print!("OK");
-					} else {
-						print!("FAIL (expected {expected_code})");
-					}
-				}
-				println!();
 			}
 		}
 	}
