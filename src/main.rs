87acc925314b04a4d16ec1d4ee3857488c0bb9e9
eightfold <97198162+8ightfold@users.noreply.github.com>
2023-02-12 15:40:58 -0700

    cli/portability tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use path_slash::PathBufExt;
-use std::{path::PathBuf, process::Command};
+use std::path::PathBuf;
 
-use tack::run::run;
+use tack::run::{run, invoke_command};
 
 fn print_help_and_exit() -> ! {
 	println!(
-		"tack compiler. very silly language
+"
+Usage: tack <input> [options...] [output]
+       tack --help (or -h)
 
-Usage: tack input [opts]
-
-  input - input file to compile
-  opts:
-    -o output - output asm file
-    -b --build - build using nasm and ld
-    -r --run - run built executable (needs --build)
+Options:
+    -h --help	  	show this text and exit
+    -o --output   	output asm file
+    -b --build 	  	build input using nasm and ld
+    -r --run 	  	run built executable (requires --build)
 "
 	);
 	std::process::exit(1);
@@ -21,22 +21,26 @@ Usage: tack input [opts]
 
 fn main() {
 	let input = match std::env::args().nth(1) {
-		Some(value) => value,
+		Some(value) =>
+			if value == "-h" || value == "--help" { print_help_and_exit() }
+			else { value },
 		_ => print_help_and_exit(),
 	};
+
 	let mut output = None;
 	let mut build = false;
 	let mut execute = false;
+
 	let mut iter = std::env::args().skip(2);
 	while let Some(arg) = iter.next() {
-		if arg == "-o" {
+		if arg == "-o" || arg == "--output" {
 			output = iter.next();
 		} else if arg == "-b" || arg == "--build" {
 			build = true;
 		} else if arg == "-r" || arg == "--run" {
 			execute = true;
 		} else {
-			println!("Unknown option \"{}\"", arg);
+			println!("Unknown option \"{arg}\"");
 			print_help_and_exit();
 		}
 	}
@@ -44,16 +48,11 @@ fn main() {
 	run(input, output.clone(), Some("graph.gv".into()), build);
 
 	if output.is_some() && build && execute {
-		let out = Command::new("bash")
-			.arg("-c")
-			.arg(format!(
-				"./{}",
-				PathBuf::from(output.unwrap()).to_slash().unwrap()
-			))
-			.output()
-			.unwrap();
+		let out = invoke_command(format!(
+			"./{}", PathBuf::from(output.unwrap()).to_slash().unwrap()
+		));
 		println!(
-			"returned code {}, output: {}",
+			"Process returned code {}, output: {}",
 			out.status.code().unwrap(),
 			String::from_utf8(out.stdout).unwrap()
 		);
