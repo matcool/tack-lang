15b58709d1238d5210377cee8dce2cf48de57e3c
matcool <26722564+matcool@users.noreply.github.com>
2025-02-13 01:28:33 -0300

    no reason for c runtime to be doing this still

--- runtime/c/tack_runtime.c
+++ runtime/c/tack_runtime.c
@@ -1,21 +1,16 @@
 #include "tack_runtime.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct str {
 	u8* data;
 	i32 size;
 };
 
-void* tack_malloc(uptr size) {
-	void* malloc(uptr);
-	return malloc(size);
-}
-void tack_free(void* ptr) {
-	void free(void*);
-	free(ptr);
-}
+void* tack_malloc(uptr size) { return malloc(size); }
+void tack_free(void* ptr) { free(ptr); }
 void* tack_memcpy(void* dst, void* src, uptr size) {
-	void* memcpy(void*, const void*, uptr);
 	return memcpy(dst, src, size);
 }
 void tack_print(struct str s) { fwrite(s.data, 1, s.size, stdout); }
